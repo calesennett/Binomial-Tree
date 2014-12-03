1b44d20ee9e40b2ab23afe915ccbaf8a5e72581e
Cale Sennett <calesennett@gmail.com>
2014-12-03 16:51:51 -0600

    add actual memoization! (and more tests)

--- Binomial.hs
+++ Binomial.hs
@@ -5,58 +5,66 @@ module Binomial
     , optionVal
     , genPrice
     , Option (..)
+    , optionMemo
     ) where
 
 import           Option
 import           Data.Matrix
-import           Data.MemoUgly
+import           Data.Maybe
+import           Data.IORef
+import           System.IO.Unsafe
+import qualified Data.Map as Map
 
 main =  do
         let option  = Option { optionType="call"
                              , stock=31.25
                              , strike=30.00
                              , riskFree=0.07
-                             , time=0.1
+                             , time=0.01
                              , vol=0.5
                              , divYield=0.0
                              }
-            steps       = 22
+            steps       = 125
             rf          = riskFree option
             dt          = deltaT option steps
             p           = prob option steps
             priceTree   = fromList steps steps $ map (genPrice steps option) (pairs [0..(steps - 1)])
             finals      = toLists $ fromList steps steps $ map (finalVals option steps priceTree) (pairs [0..(steps - 1)])
-        print $ optionVal rf dt p steps (0,0) finals (finalNodes steps)
+            val         = optionMemo (rf, dt, p, steps, (0,0), finals, (finalNodes steps))
+        print val
 
 optionVal
-    :: Double
-    -> Double
-    -> Double
-    -> Int
-    -> (Int, Int)
-    -> [[Double]]
-    -> [(Int, Int)]
+    ::  ( Double
+        , Double
+        , Double
+        , Int
+        , (Int, Int)
+        , [[Double]]
+        , [(Int, Int)])
     -> Double
 optionVal
-    rf
-    dt
-    p
-    steps
-    (downs, ups)
-    finalOpts
-    leaves =  memo optionVal' rf dt p steps (downs, ups) finalOpts leaves
-                 where
-                 optionVal'
-                    rf
-                    dt
-                    p
-                    steps
-                    (downs, ups)
-                    finalOpts
-                    leaves
-                    | (downs, ups) `elem` leaves = finalOpts !! downs !! ups
-                    | otherwise = exp (-rf * dt) * ((p * (optionVal rf dt p steps (ups+1, downs) finalOpts leaves))
-                                                                 +  ((1.0 - p) * (optionVal rf dt p steps (ups, downs+1) finalOpts leaves)))
+    ( rf
+    , dt
+    , p
+    , steps
+    , (downs, ups)
+    , finalOpts
+    , leaves)
+    | (downs, ups) `elem` leaves = finalOpts !! downs !! ups
+    | otherwise =   let upVal   = optionMemo (rf, dt, p, steps, (downs, ups+1), finalOpts, leaves)
+                        downVal = optionMemo (rf, dt, p, steps, (downs+1, ups), finalOpts, leaves)
+                    in  (exp (-rf * dt) * ((p * upVal) + ((1.0 - p) * downVal)))
+
+optionMemo
+    ::  ( Double
+        , Double
+        , Double
+        , Int
+        , (Int, Int)
+        , [[Double]]
+        , [(Int, Int)])
+    -> Double
+optionMemo = memoize optionVal
 
 finalVals
     :: Option
@@ -70,8 +78,8 @@ finalVals
     prices
     (i, j)
     | (i, j) `elem` (finalNodes steps) = if (optionType option) == "call"
-                                         then maximum [0.0, (!) prices (i+1, j+1) - strike option]
-                                         else maximum [0.0, strike option - (!) prices (i+1, j+1)]
+                                         then maximum [0, (!) prices (i+1, j+1) - strike option] -- call option
+                                         else maximum [0, strike option - (!) prices (i+1, j+1)] -- put  option
     | otherwise                        = 0
 
 finalNodes
@@ -92,6 +100,23 @@ genPrice
     option
     (i, j) = stock o * (up o steps)^^(j-i)
              where o = option
+
+{- Memoize function credit to Martin Lutke,
+ - URL: http://stackoverflow.com/questions/141650/how-do-you-make-a-generic-memoize-function-in-haskell
+ - Desc: Memoizes a function using a Map structure
+ --}
+
+memoize :: Ord a => (a -> b) -> (a -> b)
+memoize f = unsafePerformIO $ do
+    r <- newIORef Map.empty
+    return $ \ x -> unsafePerformIO $ do
+        m <- readIORef r
+        case Map.lookup x m of
+            Just y  -> return y
+            Nothing -> do
+                    let y = f x
+                    writeIORef r (Map.insert x y m)
+                    return y
 
 pairs
     :: (Eq a)
--- Tests.hs
+++ Tests.hs
@@ -1,3 +1,5 @@
+module Test.Binomial where
+
 import           Test.Hspec
 import           Binomial
 import           Option
@@ -15,7 +17,7 @@ main =  hspec $ do
                 finalNodes 4 `shouldMatchList` [(0,3), (1,2), (2,1), (3,0)]
 
         describe "finalVals" $ do
-            it "can return correct payoff at expiry nodes" $ do
+            it "can return correct payoff at expiry nodes for call" $ do
                 let matrix = M.fromList 3 3 $ [0, 0, 30,
                                                0, 25, 0,
                                                20, 0, 0]
@@ -27,11 +29,24 @@ main =  hspec $ do
                                      , vol=0.5
                                      , divYield=0.0
                                      }
-
                 map (finalVals option 3 matrix) (pairs [0..2]) `shouldMatchList` [0,0,20,0,15,0,10,0,0]
 
+            it "can return correct payoff at expiry nodes for put" $ do
+                let matrix = M.fromList 3 3 $ [0, 0, 30,
+                                               0, 25, 0,
+                                               20, 0, 0]
+                    option  = Option { optionType="put"
+                                     , stock=31.25
+                                     , strike=35.00
+                                     , riskFree=0.07
+                                     , time=0.08333333333
+                                     , vol=0.5
+                                     , divYield=0.0
+                                     }
+                map (finalVals option 3 matrix) (pairs [0..2]) `shouldMatchList` [0,0,5,0,10,0,15,0,0]
+
         describe "optionVal" $ do
-            it "can return correct option value at time 0" $ do
+            it "can return correct call option value" $ do
                 let option  = Option { optionType="call"
                                      , stock=31.25
                                      , strike=22.75
@@ -40,11 +55,51 @@ main =  hspec $ do
                                      , vol=0.5
                                      , divYield=0.0
                                      }
-                    steps   = 16
+                    steps   = 50
                     leaves  = finalNodes steps
                     prices  = M.fromList steps steps $ map (genPrice steps option) (pairs [0..(steps - 1)])
-                    finals  = M.fromList steps steps $ map (finalVals option steps prices) (pairs [0..(steps - 1)])
-                optionVal option steps (0,0) finals leaves `shouldSatisfy` within 0.005 8.649020611573942
+                    finals  = M.toLists $ M.fromList steps steps $ map (finalVals option steps prices) (pairs [0..(steps - 1)])
+                    rf      = riskFree option
+                    dt      = deltaT option steps
+                    p       = prob option steps
+                optionMemo (rf, dt, p, steps, (0,0), finals, leaves) `shouldSatisfy` within 0.001 8.649020611573942
+
+            it "can return correct put option value" $ do
+                let option  = Option { optionType="put"
+                                     , stock=31.25
+                                     , strike=35.00
+                                     , riskFree=0.07
+                                     , time=0.08333333333
+                                     , vol=0.5
+                                     , divYield=0.0
+                                     }
+                    steps   = 50
+                    leaves  = finalNodes steps
+                    prices  = M.fromList steps steps $ map (genPrice steps option) (pairs [0..(steps - 1)])
+                    finals  = M.toLists $ M.fromList steps steps $ map (finalVals option steps prices) (pairs [0..(steps - 1)])
+                    rf      = riskFree option
+                    dt      = deltaT option steps
+                    p       = prob option steps
+                optionMemo (rf, dt, p, steps, (0,0), finals, leaves) `shouldSatisfy` within 0.01 4.175469637831011
+
+            it "can compute worthless option value" $ do
+                let option  = Option { optionType="call"
+                                     , stock=31.25
+                                     , strike=33.00
+                                     , riskFree=0.07
+                                     , time=0.0833333
+                                     , vol=0.1
+                                     , divYield=0.0
+                                     }
+                    steps   = 50
+                    leaves  = finalNodes steps
+                    prices  = M.fromList steps steps $ map (genPrice steps option) (pairs [0..(steps - 1)])
+                    finals  = M.toLists $ M.fromList steps steps $ map (finalVals option steps prices) (pairs [0..(steps - 1)])
+                    rf      = riskFree option
+                    dt      = deltaT option steps
+                    p       = prob option steps
+                optionMemo (rf, dt, p, steps, (0,0), finals, leaves) `shouldSatisfy` within 1.0 0.01
+
 
 within
     :: Double
